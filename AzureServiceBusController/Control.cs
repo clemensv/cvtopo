131568326e905412319cb8817f7edf58845ac8ac
Clemens Vasters <clemensv@microsoft.com>
2019-10-09 21:24:27 +0200

    baseline

---
+++ AzureServiceBusController/Control.cs
@@ -0,0 +1,163 @@
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using Microsoft.Azure.ServiceBus.Management;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureServiceBusController
+{
+    public class Control
+    {
+        static ServiceBusConnection connection;
+        static object connectionMutex = new object();
+        static Dictionary<string, MessageSender> senders = new Dictionary<string, MessageSender>();
+
+        [FunctionName("ControlImport")]
+        [return: ServiceBus("%control-topic%", Connection = "control-cxn")]
+        public Message ControlImport([ServiceBusTrigger("%remote-control-topic%", "%remote-control-sub%", Connection = "remote-control-cxn")]
+                               Message message, ILogger log)
+        {
+            return message.Clone();
+        }
+
+        [FunctionName("Bridge")]
+        public async Task Bridge([ServiceBusTrigger("%remote-bridge-queue%", Connection = "remote-control-cxn")]
+                               Message message, ILogger log)
+        {
+            MessageSender sender;
+            var msg = message.Clone();
+            lock (connectionMutex)
+            {
+                connection = new ServiceBusConnection(Environment.GetEnvironmentVariable("control-cxn"));
+                if (!senders.TryGetValue(message.To, out sender))
+                {
+                    sender = new MessageSender(connection, message.To);
+                    senders.Add(message.To, sender);
+                }
+            }
+            msg.To = null;
+            await sender.SendAsync(msg);            
+        }
+
+        [FunctionName("ControlHandler")]
+        public async Task ControlHandlerAsync([ServiceBusTrigger("%control-topic%", "%control-sub%", Connection = "control-cxn")]
+                               Message message, ILogger log)
+        {
+            if (message.Body == null)
+                return;
+
+            ManagementClient mc = new ManagementClient(Environment.GetEnvironmentVariable("control-cxn"));
+            switch (message.Label?.ToLowerInvariant())
+            {
+                case "create":
+                    {
+                        var create = JObject.Parse(Encoding.UTF8.GetString(message.Body));
+                        var type = create.Value<string>("type");
+                        var name = create.Value<string>("name");
+                        var topic = create.Value<string>("topic");
+
+                        switch (type?.ToLowerInvariant())
+                        {
+                            case "queue":
+                                if (!await mc.QueueExistsAsync(name))
+                                {
+                                    await mc.CreateQueueAsync(name);
+                                }
+                                break;
+                            case "topic":
+                                if (!await mc.TopicExistsAsync(name))
+                                {
+                                    await mc.CreateTopicAsync(name);
+                                }
+                                if (!await mc.SubscriptionExistsAsync(name, "bridge"))
+                                {
+                                    var sd = new SubscriptionDescription(name, "bridge")
+                                    {
+                                        ForwardTo = "bridge"
+                                    };
+                                    var rl = new RuleDescription("bridge")
+                                    {
+                                        Filter = new SqlFilter("xbridge is null"),
+                                        Action = new SqlRuleAction($"set xbridge=1; set sys.to='{name}'")
+                                    };
+                                    await mc.CreateSubscriptionAsync(sd, rl);
+                                    
+                                }
+                                break;
+                            case "subscription":
+                                if (await mc.TopicExistsAsync(topic) && 
+                                    !await mc.SubscriptionExistsAsync(topic, name) )
+                                {
+                                    await mc.CreateSubscriptionAsync(topic, name);
+                                }
+                                break;
+                            case "link":
+                                if (await mc.TopicExistsAsync(topic) &&
+                                    !await mc.SubscriptionExistsAsync(topic, name) &&
+                                    await mc.QueueExistsAsync(name))
+                                {
+                                    var sd = new SubscriptionDescription(topic, name)
+                                    {
+                                        ForwardTo = name
+                                    };
+                                    await mc.CreateSubscriptionAsync(sd);
+                                }
+                                break;
+                            default:
+                                log.LogError($"Don't know how to create '{type}'");
+                                return;
+                        }
+                    }
+                    break;
+                case "delete":
+                    {
+                        var delete = JObject.Parse(Encoding.UTF8.GetString(message.Body));
+                        var type = delete.Value<string>("type");
+                        var name = delete.Value<string>("name");
+                        var topic = delete.Value<string>("topic");
+                        switch (type?.ToLowerInvariant())
+                        {
+                            case "queue":
+                                if (await mc.QueueExistsAsync(name))
+                                {
+                                    await mc.DeleteQueueAsync(name);
+                                }
+                                break;
+                            case "topic":
+                                if (await mc.TopicExistsAsync(name))
+                                {
+                                    await mc.DeleteTopicAsync(name);
+                                }
+                                break;
+                            case "subscription":
+                                if (await mc.TopicExistsAsync(topic) && 
+                                    await mc.SubscriptionExistsAsync(topic, name))
+                                {
+                                    await mc.DeleteSubscriptionAsync(topic, name);
+                                }
+                                break;
+                            case "link":
+                                if (await mc.TopicExistsAsync(topic) && 
+                                    await mc.SubscriptionExistsAsync(topic, name))
+                                {
+                                    await mc.DeleteSubscriptionAsync(topic, name);
+                                }
+                                break;
+                            default:
+                                log.LogError($"Don't know how to delete '{type}'");
+                                return;
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
+
---
+++ ServiceBusControllerClient/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBusControllerClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            var cxn = "Endpoint=sb://cvtopoweu.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ibKoXIg8F6SYQVuInImVGYqU+qva3e96AV4gTgtCk9k=";
+            var cxn1 = "Endpoint=sb://cvtoponeu.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=J5PVXWKtL4/ya7u+nYKWkZqyycvHdj9FYVfhzT7K77I=";
+
+            var tc = new TopicClient( cxn1, "control");
+
+
+
+            var entities = new List<dynamic>();
+            entities.Add(new { type = "queue", name = "lh" });
+            entities.Add(new { type = "queue", name = "dl" });
+            entities.Add(new { type = "queue", name = "aa" });
+            entities.Add(new { type = "topic", name = "a10" });
+            entities.Add(new { type = "topic", name = "a11" });
+            entities.Add(new { type = "topic", name = "a12" });
+            entities.Add(new { type = "topic", name = "a13" });
+            entities.Add(new { type = "link", topic = "a10", name = "lh" });
+            entities.Add(new { type = "link", topic = "a11", name = "lh" });
+            entities.Add(new { type = "link", topic = "a12", name = "lh" });
+
+
+            foreach (dynamic entity in entities)
+            {
+                await tc.SendAsync(
+                    new Message(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(entity)))
+                    {
+                        Label = "create"
+                    });
+            }
+
+        }
+    }
+}
